15b8131954443a0c388b514e044d93a4f8655ac8
agoktugaydin <goktugaydin36@gmail.com>
2024-02-14 13:23:58 +0300

    add DeviceData model

---
+++ backend/models/DeviceData.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const deviceSchema = new mongoose.Schema({
+    deviceId: {
+        type: String,
+        required: [true, "Please provide a device id."],
+        unique: [true, "Device id must be unique."],
+      },
+    masterId: String,
+    ownerId: String,
+    title: String,
+    type: String,
+    zone: String,
+    status: String
+});
+
+// Check if the model already exists to avoid redefining it
+const DeviceData = mongoose.models.DeviceData || mongoose.model('DeviceData', deviceSchema);
+
+module.exports = DeviceData;
