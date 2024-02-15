2267644c21b91b5879181c7296759b671fee52e9
agoktugaydin <goktugaydin36@gmail.com>
2024-02-15 12:54:18 +0300

    Add endpoint to fetch all data

--- backend/routes/dataRoutes.js
+++ backend/routes/dataRoutes.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const { fetchRecentDataFromDatabase } = require('../controllers/dataController');
+const { fetchRecentDataFromDatabase, fetchAllDataFromDatabase } = require('../controllers/dataController');
 
+// Endpoint to fetch recent data
 router.get('/data', async (req, res) => {
     try {
         const deviceId = req.query.deviceId;
@@ -10,6 +11,18 @@ router.get('/data', async (req, res) => {
         res.json(data);
     } catch (error) {
         console.error('Error fetching data:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+// Endpoint to fetch all data
+router.get('/allData', async (req, res) => {
+    try {
+        const deviceId = req.query.deviceId;
+        const data = await fetchAllDataFromDatabase(deviceId);
+        res.json(data);
+    } catch (error) {
+        console.error('Error fetching all data:', error);
         res.status(500).json({ error: 'Internal Server Error' });
     }
 });
