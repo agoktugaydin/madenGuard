6fccebb05bef6b0a39920fa4d8b83e34cd7b11b7
agoktugaydin <goktugaydin36@gmail.com>
2024-03-16 22:45:11 +0300

    refactor dbConfig

--- backend/config/dbConfig.js
+++ backend/config/dbConfig.js
@@ -1,10 +1,9 @@
 const mongoose = require('mongoose');
 
 function connectToMongoDB() {
-    mongoose.connect('mongodb://localhost:27017/madenGuardDB');
-
+    mongoose.connect(process.env.MONGO_URI);
     const db = mongoose.connection;
-
+    
     db.on('error', console.error.bind(console, 'MongoDB connection error:'));
     db.once('open', () => {
         console.log('Connected to MongoDB');
