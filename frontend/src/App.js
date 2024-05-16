3121ff5e13c399ed48e0657015f09a032b9d9aba
agoktugaydin <goktugaydin36@gmail.com>
2024-05-16 18:42:47 +0300

    add customer/graph pages  and  routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,130 +1,96 @@
-// src/App.js
 import React, { useState, useEffect } from 'react';
-import HomePage from '../src/pages/HomePage';
-import DeviceLogPage from '../src/pages/DeviceLogPage';
-import RegisterDevicePage from '../src/pages/RegisterDevicePage';
-import ListDevicesPage from '../src/pages/ListDevicesPage';
-import ListCustomersPage from '../src/pages/ListCustomersPage';
-import RegisterCustomerPage from '../src/pages/RegisterCustomerPage';
+import { Link, BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { AppBar, Container, Button, Toolbar } from '@mui/material';
+import HomePage from './pages/HomePage';
+import DeviceLogPage from './pages/DeviceLogPage';
+import RegisterDevicePage from './pages/RegisterDevicePage';
+import ListDevicesPage from './pages/ListDevicesPage';
+import ListCustomersPage from './pages/ListCustomersPage';
+import RegisterCustomerPage from './pages/RegisterCustomerPage';
 import AllDevicesPage from './pages/DashBoardPage';
-import MapPage from './pages/MapPage'
-
-import './App.css';
-
-import {
-  Link,
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  NavLink,
-  Navigate
-} from "react-router-dom";
-
-import { AppBar, Tabs, Tab, Container, Button } from '@mui/material';
+import MapPage from './pages/MapPage';
+import GraphPage from './pages/GraphPages/GraphPage';
+import GraphEditor from './pages/GraphPages/GraphEditor';
 import DeviceDetailsPage from './pages/DeviceDetailsPage';
 import EditDevicePage from './pages/EditDevicePage';
 import LoginPage from './pages/LoginPage';
+import CustomerDetailsPage from './pages/CustomerDetailsPage';
+import EditCustomerPage from './pages/EditCustomerPage';
+
+import './App.css';
 
 const App = () => {
-  const [isLoggedIn, setLoggedIn] = useState(false); // Track login state
+  const [isLoggedIn, setLoggedIn] = useState(false);
 
-  // Function to handle logout
   const handleLogout = () => {
-    // Perform logout actions, e.g., clear token, reset state, etc.
-    // For now, let's just set isLoggedIn to false
     setLoggedIn(false);
-    // Clear the token from localStorage
     localStorage.removeItem('token');
     localStorage.removeItem('role');
-    // Navigate to the login page after logout
-    return <Navigate to="/login" />;
   };
 
   useEffect(() => {
-    const role = localStorage.getItem('role');
     const token = localStorage.getItem('token');
     if (token) {
       setLoggedIn(true);
     }
-  }, []); // Empty dependency array ensures the effect runs only once on mount
+  }, []);
 
-  const isAdmin = localStorage.getItem('role') === 'ROLE_ADMIN'; // Check if the user is an admin
+  const isAdmin = localStorage.getItem('role') === 'ROLE_ADMIN';
 
-  const navbarStyle = {
-    backgroundColor: '#2196f3', // '#2196f3', // primary color
-    padding: '10px 0',
-  };
-
-  const containerStyle = {
-    display: 'flex',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-  };
-
-  const tabStyle = {
-    flexGrow: 1,
-    marginLeft: '16px',
-  };
-
-  const tabItemStyle = {
-    marginRight: '16px',
-    color: 'black',
-    textDecoration: 'none',
-  };
-
-  const tabItemStyleDisabled = {
-    marginRight: '16px',
-    color: 'black',
-    textDecoration: 'line-through',
-  };
-
-  const logoutButtonStyle = {
-    color: 'black',
-    borderColor: 'white',
-    marginLeft: '16px',
+  const PrivateRoute = ({ element }) => {
+    return isLoggedIn ? element : <Navigate to="/login" />;
   };
 
   return (
     <Router>
-
-      <AppBar style={navbarStyle} position="static">
-        <Container style={containerStyle}>
-          <Tabs style={tabStyle}>
-            <Tab style={tabItemStyle} label="Home" component={Link} to="/" />
-            <Tab style={tabItemStyle} label="Device Log" component={Link} to="/device-log" />
-            <Tab style={tabItemStyle} label="Map" component={Link} to="/map" />
-            <Tab style={tabItemStyle} label="Dashboard" component={Link} to="/all-devices" />
-            <Tab style={tabItemStyle} label="Device List" component={Link} to="/device-list" />
-            <Tab style={tabItemStyle} label="Device Register" component={Link} to="/device-register" />
-            {isAdmin && <Tab style={tabItemStyle} label="Customer List" component={Link} to="/customer-list" />}
-            {isAdmin && <Tab style={tabItemStyle} label="Customer Register" component={Link} to="/customer-register" />}
-          </Tabs>
-          {isLoggedIn && (
-            <Button style={logoutButtonStyle} color="inherit" onClick={handleLogout}>
-              Logout
-            </Button>
-          )}
+      <AppBar className="navbar" position="static">
+        <Container className="navbar-container">
+          <Toolbar>
+            <Button component={Link} to="/" color="inherit">Home</Button>
+            {isLoggedIn && (
+              <>
+                <Button component={Link} to="/device-log" color="inherit">Device Log</Button>
+                <Button component={Link} to="/graph-editor" color="inherit">Graph Editor</Button>
+                <Button component={Link} to="/graph" color="inherit">Graph</Button>
+                <Button component={Link} to="/all-devices" color="inherit">Dashboard</Button>
+                <Button component={Link} to="/device-list" color="inherit">Device List</Button>
+                <Button component={Link} to="/device-register" color="inherit">Device Register</Button>
+                {isAdmin && <Button component={Link} to="/customer-list" color="inherit">Customer List</Button>}
+                {isAdmin && <Button component={Link} to="/customer-register" color="inherit">Customer Register</Button>}
+              </>
+            )}
+            {isLoggedIn ? (
+              <Button className="logout-button" color="inherit" onClick={handleLogout}>Logout</Button>
+            ) : (
+              <Button component={Link} to="/login" color="inherit">Login</Button>
+            )}
+          </Toolbar>
         </Container>
       </AppBar>
 
-      <Routes>
-        <Route path="/" exact element={<HomePage isLoggedIn={isLoggedIn} />} />
-        <Route path="/login" exact element={<LoginPage setLoggedIn={setLoggedIn} isLoggedIn={isLoggedIn} />} />
-        <Route path="/device-log" exact element={<DeviceLogPage isLoggedIn={isLoggedIn} />} />
-        <Route path="/device-list" exact element={<ListDevicesPage isLoggedIn={isLoggedIn} />} />
-        <Route path="/device-register" exact element={<RegisterDevicePage isLoggedIn={isLoggedIn} />} />
-        <Route path="/device-edit/:deviceId" exact element={<EditDevicePage isLoggedIn={isLoggedIn} />} />
-        <Route path="/device-details/:deviceId" exact element={<DeviceDetailsPage isLoggedIn={isLoggedIn} />} />
+      <Container className="main-container">
+        <Routes>
+          <Route path="/" element={<HomePage isLoggedIn={isLoggedIn} />} />
+          <Route path="/login" element={<LoginPage setLoggedIn={setLoggedIn} isLoggedIn={isLoggedIn} />} />
+          <Route path="/device-log" element={<PrivateRoute element={<DeviceLogPage isLoggedIn={isLoggedIn} />} />} />
+          <Route path="/graph-editor" element={<PrivateRoute element={<GraphEditor isLoggedIn={isLoggedIn} />} />} />
+          <Route path="/graph" element={<PrivateRoute element={<GraphPage isLoggedIn={isLoggedIn} />} />} />
+          <Route path="/all-devices" element={<PrivateRoute element={<AllDevicesPage isLoggedIn={isLoggedIn} />} />} />
+          <Route path="/device-list" element={<PrivateRoute element={<ListDevicesPage isLoggedIn={isLoggedIn} />} />} />
+          <Route path="/device-register" element={<PrivateRoute element={<RegisterDevicePage isLoggedIn={isLoggedIn} />} />} />
+          <Route path="/device-edit/:deviceId" element={<PrivateRoute element={<EditDevicePage isLoggedIn={isLoggedIn} />} />} />
+          <Route path="/device-details/:deviceId" element={<PrivateRoute element={<DeviceDetailsPage isLoggedIn={isLoggedIn} />} />} />
+          <Route path="/map" element={<PrivateRoute element={<MapPage isLoggedIn={isLoggedIn} />} />} />
+          <Route path="/customer-list" element={<PrivateRoute element={<ListCustomersPage isLoggedIn={isLoggedIn} />} />} />
+          <Route path="/customer-register" element={<PrivateRoute element={<RegisterCustomerPage isLoggedIn={isLoggedIn} />} />} />
 
-        <Route path="/all-devices" exact element={<AllDevicesPage isLoggedIn={isLoggedIn} />} />
-        <Route path="/map" exact element={<MapPage isLoggedIn={isLoggedIn} />} />
+          <Route path="/customer-details/:customerId" element={<PrivateRoute element={<CustomerDetailsPage isLoggedIn={isLoggedIn} />} />} />
+          <Route path="/customer-edit/:customerId" element={<PrivateRoute element={<EditCustomerPage isLoggedIn={isLoggedIn} />} />} />
 
-        <Route path="/customer-list" exact element={<ListCustomersPage isLoggedIn={isLoggedIn} />} />
-        <Route path="/customer-register" exact element={<RegisterCustomerPage isLoggedIn={isLoggedIn} />} />
-      </Routes>
+        </Routes>
+      </Container>
     </Router>
   );
 };
-
 
 export default App;
